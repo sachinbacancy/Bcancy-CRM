3a6ae5a74df6c281aaed94af50329346879ba3e1
yash-patel-bacancy <yash.patel@bacancy.com>
2021-08-26 13:58:53 +0530

    Leads Table Created

--- src/app/modules/leads/leads.module.ts
+++ src/app/modules/leads/leads.module.ts
@@ -3,7 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LeadsComponent } from './leads.component';
 import { RouterModule } from '@angular/router';
 import { LayoutsModule } from 'src/app/layouts/layouts.module';
-
+import { CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
+import { ToolbarModule } from '@syncfusion/ej2-angular-navigations';
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PageService, FilterService, SortService, SelectionService, GridModule, PagerModule, EditService, ToolbarService, CommandColumnService, SearchService } from '@syncfusion/ej2-angular-grids';
+import { DateRangePickerModule } from '@syncfusion/ej2-angular-calendars';
 
 
 @NgModule({
@@ -11,9 +16,17 @@ import { LayoutsModule } from 'src/app/layouts/layouts.module';
   imports: [
     CommonModule,
     LayoutsModule,
+    GridModule,
+    PagerModule,
+    ToolbarModule,
+    DialogModule,
+    CheckBoxModule,
+    DateRangePickerModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       { path: '', component: LeadsComponent },
     ])
-  ]
+  ],
+  providers:[PageService, FilterService, SortService, SelectionService, EditService, ToolbarService, SearchService, CommandColumnService]
 })
 export class LeadsModule { }
